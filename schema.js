1982fc157327c283a92012962040992ccaa04e96
Gabriela Paredes Paredes <gabiprds.93@gmail.com>
2019-11-10 10:33:24 -0500

    add types, queries and mutations in schema

--- schema.js
+++ schema.js
@@ -1,14 +1,30 @@
 const {buildSchema } = require('graphql');
 
 const schema = buildSchema(`
-  type Amiibo {
+  type Query {
+    products: [Product]
+    orders: [Order]
+  }
+  
+  type Mutation {
+    addOrder (
+      id: ID!
+      delivery_date: String
+      products: [Product]
+    ):
+  }
+
+  type Product {
     id: ID
     image: String
     name: String
     price: Int
   }
-  type Query {
-    amiibos: [Amiibo]
+
+  type Order {
+    id: ID
+    delivery_date: String
+    products: [Product]
   }
 `);
 
